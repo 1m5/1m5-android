71d1880a9ad6eb5403ba9bd6307035bcee0063aa
objectorange <brian@resolvingarchitecture.io>
2020-06-22 21:09:02 -0700

    add service bus intent and aidl

---
+++ app/src/main/java/onemfive/android/ServiceBusIntent.java
@@ -0,0 +1,95 @@
+package onemfive.android;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+import ra.servicebus.ServiceBus;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class ServiceBusIntent extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_START_BUS = "onemfive.android.action.START_BUS";
+    private static final String ACTION_STOP_BUS = "onemfive.android.action.STOP_BUS";
+
+    // TODO: Rename parameters
+    private static final String EXTRA_PARAM1 = "onemfive.android.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "onemfive.android.extra.PARAM2";
+
+    private ServiceBus serviceBus;
+
+    public ServiceBusIntent() {
+        super("ServiceBusIntent");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startBus(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, ServiceBusIntent.class);
+        intent.setAction(ACTION_START_BUS);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void stopBus(Context context, String param1, String param2) {
+        Intent intent = new Intent(context, ServiceBusIntent.class);
+        intent.setAction(ACTION_STOP_BUS);
+        intent.putExtra(EXTRA_PARAM1, param1);
+        intent.putExtra(EXTRA_PARAM2, param2);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_START_BUS.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionFoo(param1, param2);
+            } else if (ACTION_STOP_BUS.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
+                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
+                handleActionBaz(param1, param2);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionFoo(String param1, String param2) {
+        // TODO: Handle action Foo
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
